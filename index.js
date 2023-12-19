9477da873f096f08b4a37ea4d046c924a8187b5e
Berna Murad <bmurad@aiopsgroup.com>
2023-12-19 13:28:21 +0200

    examples

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ var expect = function (val) {
   };
 };
 
+try {
+  expect(5).toBe(5);
+  expect(5).notToBe(5);
+} catch (error) {
+  console.error(error.message);
+}
+
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
